7ed4614fc2a6abf202ed125259bf136b2ca0c147
Universumgames <tommyios.ta@gmail.com>
2020-03-24 00:35:03 +0100

    tested some things and changed default main file

---
+++ src/Messenger.vue
@@ -0,0 +1,26 @@
+<template>
+   <div id="app">
+      <Nav/>
+      <router-view/>
+      <h1>hi</h1>
+   </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Nav from './components/Nav.vue';
+
+@Component({
+  components: {
+     Nav,
+  },
+})
+export default class Messenger extends Vue {}
+</script>
+
+<style>
+#app{
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   color: rgb(66, 219, 66);
+}
+</style>
