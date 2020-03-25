e0eabc20af317ba300ca091817d21422196fa49a
Universumgames <tommyios.ta@gmail.com>
2020-03-25 13:39:26 +0100

    Revert "added apollo"
    This reverts commit bf0c596dd110b72320cd115cc99b6832c1917c22.

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import Messenger from './Messenger.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
   render: h => h(Messenger)
 }).$mount('#app')
